a910350cb34f7d6a18e3ae0e30214bdcf0d765f1
mel <romelcub@gmail.com>
2021-09-11 15:04:39 +0800

    db update, product updated

--- application/views/admin/modals/add_product.php
+++ application/views/admin/modals/add_product.php
@@ -8,26 +8,41 @@
 				<div class="modal-body">
 					<div class="form-row text-center d-flex flex-wrap justify-content-center">
 						<div class="form-group col-12 col-sm-12 col-md-11 mb-4">
-							<input id="in-productcode" type="text" class="form-control" name="product-code" autocomplete="off">
+							<input id="in-productcode" type="text" class="form-control text-center" name="product-code" autocomplete="off">
 							<label class="input-label" for="in-productcode">ITEM CODE</label>
 						</div>
 						<div class="form-group col-12 col-sm-12 col-md-11 mb-4">
-							<input id="in-productname" type="text" class="form-control" name="product-name">
+							<input id="in-productname" type="text" class="form-control text-center" name="product-name" autocomplete="off">
 							<label class="input-label" for="in-productname">ITEM NAME</label>
 						</div>
 						<div class="form-group col-12 col-sm-12 col-md-5 col-md-11 m-auto mb-4">
-							<select id="in-productcategory" class="form-control" name="product-category">
+							<select id="in-productcategory" class="form-control text-center" name="product-category" autocomplete="off">
 								<option selected="">N/A</option>
-								<option> Category 1</option>
-								<option> Category 2</option>
+								<option>Accessories</option>
+								<option>Food</option>
+								<option>Out wear</option>
 							</select>
 							<label class="input-label" for="in-productcategory">ITEM CATEGORY</label>
 						</div>
 					</div>
 					<div class="form-row text-center d-flex flex-wrap">
 						<div class="form-group col-12 col-sm-12 col-md-11 m-auto mb-4">
-							<textarea class="form-control" name="product-description" rows="5"></textarea>
+							<textarea class="form-control" name="product-description" rows="5" autocomplete="off"></textarea>
 							<label class="input-label">DESCRIPTION</label>
+						</div>
+					</div>
+					<div class="form-row text-center d-flex flex-wrap">
+						<div class="form-group col-12 col-sm-12 col-md-3 m-auto mb-4">
+							<input class="form-control text-center" type="text" name="pro_weight" autocomplete="off">
+							<label class="input-label">WEIGHT</label>
+						</div>
+						<div class="form-group col-12 col-sm-12 col-md-3 m-auto mb-4">
+							<input class="form-control text-center num-noarrow" type="number" name="prc_pitem" autocomplete="off">
+							<label class="input-label">PRICE</label>
+						</div>
+						<div class="form-group col-12 col-sm-12 col-md-3 m-auto mb-4">
+							<input class="form-control text-center num-noarrow" type="number" name="cst_pitem" autocomplete="off">
+							<label class="input-label">COST PER ITEM</label>
 						</div>
 					</div>
 				</div>
